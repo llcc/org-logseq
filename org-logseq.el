915f00883c76354aa62d62df6c1829a7d283c176
leizhe <lzhes43@gmail.com>
2021-04-02 07:25:51 +0900

    fix: block ref issues

--- org-logseq.el
+++ org-logseq.el
@@ -84,13 +84,15 @@ The type can be 'url, 'draw and 'page, denoting the link type."
         ('draw (org-logseq-open-excalidraw link))
         (_ (let ((result (shell-command-to-string
                           (org-logseq-grep-query t-l))))
-             (if (string= result "")
-                 (org-logseq-new-page link)
-               (let* ((f-n (split-string result ":" nil))
-                      (fname (car f-n))
-                      (lineno (string-to-number (cadr f-n))))
-                 (delete-other-windows)
-                 (org-open-file fname t lineno)))))))))
+             (when (string-prefix-p "grep" result)
+               (error "grep searching error" )
+               (if (string= result "")
+                   (org-logseq-new-page link)
+                 (let* ((f-n (split-string result ":" nil))
+                        (fname (car f-n))
+                        (lineno (string-to-number (cadr f-n))))
+                   (delete-other-windows)
+                   (org-open-file fname t lineno))))))))))
 
 (defun org-logseq-new-page (page)
   "Create a new PAGE org file in pages directory if setting
@@ -147,10 +149,11 @@ The type can be 'url, 'draw and 'page, denoting the link type."
 
 ;;; Logseq id overlays
 (defvar-local org-logseq-block-ref-overlays nil)
+(defvar-local org-logseq-block-embed-overlays nil)
 (defvar-local org-logseq-buffer-modified-p nil)
 
 (defvar org-logseq-block-ref-re "((\\([a-zA-Z0-9-]+\\)))")
-(defvar org-logseq-block-embed-re "{{embed +((\\([a-zA-Z0-9-]+\\)))]}}")
+(defvar org-logseq-block-embed-re "{{embed +((\\([a-zA-Z0-9-]+\\))) *}}")
 
 (defface org-logseq-ref-block
   '((((class color) (min-colors 88) (background light))
@@ -160,7 +163,7 @@ The type can be 'url, 'draw and 'page, denoting the link type."
   "Face for transcluded block."
   :group 'org-logseq)
 
-(defface org-logseq-embeded-block
+(defface org-logseq-embed-block
   '((((class color) (min-colors 88) (background light))
      :background "#f3f3ff" :extend t)
     (((class color) (min-colors 88) (background dark))
@@ -171,65 +174,87 @@ The type can be 'url, 'draw and 'page, denoting the link type."
 (defun org-logseq-toggle-block-ref-overlays ()
   (interactive)
   (if org-logseq-block-ref-overlays
-      (org-logseq-deactivate)
-    (org-logseq-activate)))
+      (org-logseq-block-deactivate 'ref)
+    (org-logseq-block-activate 'ref)))
 
-(defun org-logseq-activate ()
-  (org-logseq-make-block-ref-overlays)
-  (add-hook 'before-save-hook 'org-logseq-remove-block-ref-overlays nil t)
-  (add-hook 'after-save-hook 'org-logseq-make-block-ref-overlays nil t)
-  (add-hook 'kill-buffer-hook 'org-logseq-remove-block-ref-overlays t))
+(defun org-logseq-toggle-block-embed-overlays ()
+  (interactive)
+  (if org-logseq-block-embed-overlays
+      (org-logseq-block-deactivate 'embed)
+    (org-logseq-block-activate 'embed)))
 
-(defun org-logseq-deactivate ()
-  (org-logseq-remove-block-ref-overlays)
-  (remove-hook 'before-save-hook 'org-logseq-remove-block-ref-overlays t)
-  (remove-hook 'after-save-hook 'org-logseq-make-block-ref-overlays t)
-  (remove-hook 'kill-buffer-hook 'org-logseq-remove-block-ref-overlays t))
+(defun org-logseq-block-activate (type)
+  (org-logseq-make-block-overlays type)
+  (add-hook 'before-save-hook #'(lambda () (org-logseq-remove-block-overlays type)) nil t)
+  (add-hook 'after-save-hook #'(lambda () (org-logseq-make-block-overlays type)) nil t)
+  (add-hook 'kill-buffer-hook #'(lambda () (org-logseq-remove-block-overlays type)) nil t))
 
-(defun org-logseq-make-block-ref-overlays (&optional beg end)
+(defun org-logseq-block-deactivate (type)
+  (org-logseq-remove-block-overlays type)
+  (remove-hook 'before-save-hook #'(lambda () (org-logseq-remove-block-overlays type)) t)
+  (remove-hook 'after-save-hook #'(lambda () (org-logseq-make-block-overlays type)) t)
+  (remove-hook 'kill-buffer-hook #'(lambda () (org-logseq-remove-block-overlays type)) t))
+
+(defun org-logseq-make-block-overlays (type &optional beg end)
   (setq org-logseq-buffer-modified-p (buffer-modified-p))
   (save-excursion
     (goto-char (or beg (point-min)))
-    (while (re-search-forward org-logseq-block-ref-re end t)
-      (let ((overlay-end (point))
-            (overlay-beg (re-search-backward "((" (line-beginning-position) t)))
-        (org-logseq-create-block-ref-overlay overlay-beg overlay-end))))
+    (let ((re (pcase type
+                ('ref org-logseq-block-ref-re)
+                ('embed org-logseq-block-embed-re))))
+      (while (re-search-forward re end t)
+        (let* ((uuid (match-string-no-properties 1))
+               (tuuid (cons 'id uuid))
+               (overlay-end (point))
+               (overlay-beg (re-search-backward
+                             (pcase type ('ref "((") ('embed "{{"))
+                             (line-beginning-position) t))
+               (file-type-block (org-logseq-get-block-content tuuid type)))
+          (org-logseq-create-block-ref-overlay overlay-beg overlay-end file-type-block)))))
   (set-buffer-modified-p org-logseq-buffer-modified-p))
 
-(defun org-logseq-create-block-ref-overlay (beg end)
-  (let* ((tuuid (org-logseq-get-block-id beg end))
-         (src-fh (org-logseq-get-block-ref-heading tuuid))
-         (file (car src-fh))
-         (raw-heading (let ((result (cdr src-fh)))
-                        (dolist (func org-logseq-block-ref-heading-hook) 
-                          (setq result (funcall func result)))
-                        result))
-         (heading (format "[[id:%s][%s]]" (cdr tuuid) raw-heading)))
+(defun org-logseq-create-block-ref-overlay (beg end file-type-block)
+  (pcase-let ((`((,file . ,uuid) . (,type . ,content))
+               file-type-block))
     (delete-region beg end)
-    (insert heading)
+    (insert content)
     (let* ((end (point))
-           (ov (make-overlay beg end)))
-      (overlay-put ov 'type 'block)
+           (ov (make-overlay beg end))
+           (face (pcase type
+                   ('ref 'org-logseq-ref-block)
+                   ('embed 'org-logseq-embed-block))))
+      (overlay-put ov 'type type)
       (overlay-put ov 'evaporate t)
       (overlay-put ov 'file file)
-      (overlay-put ov 'block-uuid (cdr tuuid))
-      (overlay-put ov 'block-heading heading)
+      (overlay-put ov 'block-uuid uuid)
+      (overlay-put ov 'target content)
       (overlay-put ov 'help-echo
                    (format "Original page: %s.org" (file-name-base file)))
-      (overlay-put ov 'face 'org-logseq-ref-block)
+      (overlay-put ov 'face face)
       (add-text-properties beg end '(read-only t))
-      (push ov org-logseq-block-ref-overlays))))
+      (pcase type
+        ('ref (push ov org-logseq-block-ref-overlays))
+        ('embed (push ov org-logseq-block-embed-overlays))))))
 
-(defun org-logseq-get-block-ref-heading (tuuid)
+(defun org-logseq-get-block-content (tuuid type)
   (when-let ((result (shell-command-to-string
                       (org-logseq-grep-query tuuid))))
     (let* ((f-n (split-string result ":" nil))
            (file (car f-n))
            (line (string-to-number (cadr f-n))))
-      (cons file
+      (cons (cons file (cdr tuuid))
             (with-current-buffer (find-file-noselect file)
               (goto-line line)
-              (org-no-properties (org-get-heading)))))))
+              (cons type
+                    (pcase type
+                      ('ref (format "[[id:%s][%s]]" (cdr tuuid)
+                                    (let ((result (org-no-properties (org-get-heading))))
+                                      (dolist (func org-logseq-block-ref-heading-hook) 
+                                        (setq result (funcall func result)))
+                                      result)))
+                      ('embed 
+                       (org-narrow-to-subtree)
+                       (buffer-substring (point-min) (point-max))))))))))
 
 (defun olih-link (heading)
   (if (string-match "\\[\\[.+\\]\\[\\(.+\\)\\]\\]" heading)
@@ -240,22 +265,28 @@ The type can be 'url, 'draw and 'page, denoting the link type."
   '(olih-link)
   "Hook for cleaning up id heading")
 
-(defun org-logseq-remove-block-ref-overlays ()
+(defun org-logseq-remove-block-overlays (type)
   (setq org-logseq-buffer-modified-p (buffer-modified-p))
-  (when org-logseq-block-ref-overlays
+  (when-let ((overlays (pcase type
+                         ('ref org-logseq-block-ref-overlays)
+                         ('embed org-logseq-block-embed-overlays))))
     (save-excursion
-      (dolist (ov org-logseq-block-ref-overlays)
-        (let ((beg (overlay-start ov))
-              (end (overlay-end ov))
-              (block-uuid (overlay-get ov 'block-uuid))
-              (file (overlay-get ov 'file))
-              (inhibit-read-only t))
+      (dolist (ov overlays)
+        (let* ((beg (overlay-start ov))
+               (end (overlay-end ov))
+               (block-uuid (overlay-get ov 'block-uuid))
+               (file (overlay-get ov 'file))
+               (inhibit-read-only t))
           (remove-text-properties beg end '(read-only t))
           (delete-region beg end)
           (delete-overlay ov)
           (goto-char beg)
-          (insert (concat "((" block-uuid "))") ))))
-    (setq org-logseq-block-ref-overlays nil))
+          (insert (pcase type
+                    ('ref (concat "((" block-uuid "))"))
+                    ('embed (format "{{embed  ((%s))}}" block-uuid))))))
+      (pcase type
+        ('ref (setq org-logseq-block-ref-overlays nil))
+        ('embed (setq org-logseq-block-embed-overlays nil)))))
   (set-buffer-modified-p org-logseq-buffer-modified-p))
 
 (defvar org-logseq-map
